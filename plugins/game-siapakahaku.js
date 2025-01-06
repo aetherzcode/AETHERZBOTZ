0e1309b5dc1dc47c9d02c5c9c87b47c0be3a917e
AETHER <aetherzcode@gmail.com>
2025-01-06 12:54:30 +0700

    Update file plugins/game-siapakahaku.js

--- plugins/game-siapakahaku.js
+++ plugins/game-siapakahaku.js
@@ -1,18 +1,27 @@
-let fetch = require('node-fetch')
+let fetch = require('node-fetch');
 
-let timeout = 100000
-let poin = 10000
+let timeout = 100000;
+let poin = 10000;
+let src;
 let handler = async (m, { conn, usedPrefix }) => {
-    conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
-    let id = m.chat
+    conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {};
+    let id = m.chat;
     if (id in conn.siapakahaku) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.siapakahaku[id][0])
-        throw false
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.siapakahaku[id][0]);
+        throw false;
     }
-    // di sini dia ngambil data dari api
-    let src = await (await fetch(`https://api.betabotz.eu.org/api/game/siapakahaku?apikey=${lann}`)).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-    // buat caption buat di tampilin di wa
+    
+    // Mengambil data dari URL yang diberikan
+    if (!src) {
+        const response = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/siapakahaku.json');
+        src = await response.json();
+    }
+    
+    // Memilih karakter secara acak
+    let json = src[Math.floor(Math.random() * src.length)];
+    if (!json) throw "Terjadi kesalahan, ulangi lagi perintah!";
+    
+    // Buat caption untuk ditampilkan di WhatsApp
     let caption = `
 ${json.soal}
 
@@ -22,20 +31,22 @@ ${json.soal}
 ▢ Bonus: ${poin} money
 ▢ *Balas/ replay soal ini untuk menjawab*
 └──────────────
-`.trim()
+    `.trim();
+    
     conn.siapakahaku[id] = [
         await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.siapakahaku[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.siapakahaku[id][0])
-            delete conn.siapakahaku[id]
+            if (conn.siapakahaku[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.siapakahaku[id][0]);
+            delete conn.siapakahaku[id];
         }, timeout)
-    ]
+    ];
 }
-handler.help = ['siapakahaku']
-handler.tags = ['game']
-handler.command = /^siapakahaku/i
-handler.register = false
-handler.group = true
 
-module.exports = handler
+handler.help = ['siapakahaku'];
+handler.tags = ['game'];
+handler.command = /^siapakahaku/i;
+handler.register = false;
+handler.group = true;
+
+module.exports = handler;
