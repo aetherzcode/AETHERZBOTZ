4b757c4e947cddc99b662529dda6c921aadd3101
AETHER <aetherzcode@gmail.com>
2025-01-06 08:54:28 +0700

    Update file plugins/game-tebakkata_hint.js

--- plugins/game-tebakkata_hint.js
+++ plugins/game-tebakkata_hint.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn }) => {
-    conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
-    let id = m.chat
-    if (!(id in conn.tebakkata)) throw false
-    let json = conn.tebakkata[id][1]
-    let ans = json.jawaban
-    // kalau ini error clue nya ak mau ada tanda (_) nya ganti string dalam function di bawah ini jadi huruf kecil
-    let clue = ans.replace(/[BCDFGHJKLMNPQRSTFWXYZ]/g, '_')
-    m.reply('```' + clue + '```')
-}
-handler.command = /^kata/i
-handler.limit = true
-module.exports = handler
+    conn.tebakkata = conn.tebakkata ? conn.tebakkata : {};
+    let id = m.chat;
+    if (!(id in conn.tebakkata)) {
+        m.reply('Tidak ada soal yang sedang berlangsung di chat ini.');
+        return;
+    }
 
-//gh: dana_putra13
+    let json = conn.tebakkata[id][1]; 
+    let clue = json.jawaban.replace(/[AIUEOaiueo]/g, '_'); 
+    conn.reply(m.chat, `Petunjuk: ${clue}`, conn.tebakkata[id][0]);
+};
+
+handler.command = /^kata$/i; 
+module.exports = handler;
