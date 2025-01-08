518f393204b863dee830bad30090602796c1877a
AETHER <aetherzcode@gmail.com>
2025-01-08 12:34:58 +0700

    Update file plugins/owner-create_direktori.js

--- plugins/owner-create_direktori.js
+++ plugins/owner-create_direktori.js
@@ -1,36 +1,42 @@
 let fs = require('fs');
-let path = require('path')
-let util = require('util')
+let path = require('path');
 
 let handler = async (m, { text, usedPrefix, command }) => {
 
   if (!text) throw `ehemm.. teksnya mana jir?\n\npenggunaan:\n${usedPrefix + command} <nama direktori>\n\ncontoh:\n${usedPrefix + command} <nama direktori>`;
 
+  const rootDir = path.join(__dirname, '..', 'container'); // Folder root/container
+
   if (command === 'tambahdir') {
 
-    let newDir = `${text}`;
+    let newDir = path.join(rootDir, text); // Gabungkan jalur root/container dengan nama direktori yang diberikan
 
-    await fs.mkdirSync(newDir);
-
-    m.reply(`direktori ${newDir} berhasil ditambahkan`);
+    if (!fs.existsSync(newDir)) {
+      fs.mkdirSync(newDir, { recursive: true }); // Membuat direktori
+      m.reply(`Direktori *${newDir}* berhasil ditambahkan.`);
+    } else {
+      m.reply(`Direktori *${newDir}* sudah ada.`);
+    }
 
   } else if (command === 'hapusdir') {
 
-    let dirPath = `${text}`;
+    let dirPath = path.join(rootDir, text); // Gabungkan jalur root/container dengan nama direktori yang akan dihapus
 
-    if (!fs.existsSync(dirPath)) throw `direktori ${text} tidak ditemukan`;
+    if (!fs.existsSync(dirPath)) {
+      throw `Direktori *${text}* tidak ditemukan.`;
+    }
 
-    fs.rmdirSync(dirPath, { recursive: true });
+    fs.rmSync(dirPath, { recursive: true, force: true }); // Menghapus direktori beserta isinya
 
-    m.reply(`direktori ${text} dan isinya berhasil dihapus`);
+    m.reply(`Direktori *${text}* dan isinya berhasil dihapus.`);
 
   }
 
 };
 
-handler.help = ['mkdir', 'rmdir'].map(v => v + ' <nama direktori>');
+handler.help = ['tambahdir', 'hapusdir'].map(v => v + ' <nama direktori>');
 
 handler.tags = ['owner'];
-handler.command = /^(mkdir|rmdir)$/i;
+handler.command = /^(tambahdir|hapusdir)$/i;
 handler.rowner = true;
 module.exports = handler;
