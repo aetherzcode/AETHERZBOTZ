bb080d81b5376402ee0d6ed38a203800f675c8e2
AETHER <aetherzcode@gmail.com>
2025-01-09 19:30:31 +0700

    Update file plugins/downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,28 +1,30 @@
 const fetch = require('node-fetch');
+const mime = require('mime-types');
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `*Example:* ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`;
-    
+
     try {
         const response = await fetch(`https://api.agatz.xyz/api/mediafire?url=${args[0]}`);
         const json = await response.json();
-        
+
         if (!json.result) throw 'Failed to fetch!';
-        
+
         let { url, filename, ext, upload_date: aploud, filesize, filesizeH } = json.result;
-        
+        let mimetype = mime.lookup(ext) || 'application/octet-stream';
+
         let caption = `
-*💌 Name:* ${filename}
-*📊 Size:* ${filesizeH}
-*🗂️ Extension:* ${ext}
-*📨 Uploaded:* ${aploud}
+💌 Name: ${filename}
+📊 Size: ${filesizeH}
+🗂 Extension: ${ext}
+📨 Uploaded: ${aploud}
 `.trim();
-        
+
         m.reply(caption);
-        conn.sendMessage(m.chat, { document: { url: url }, mimetype: ext, fileName: filename }, { quoted: m });
-        
+        conn.sendMessage(m.chat, { document: { url: url }, mimetype: mimetype, fileName: filename }, { quoted: m });
+
     } catch (e) {
-        throw eror
+        throw e;
     }
 };
 
