e13fe3df85c4a54da5a8c89fd6a1618632718392
AETHER <aetherzcode@gmail.com>
2025-01-09 19:27:17 +0700

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,21 +1,35 @@
-let { mediafiredl } = require('@bochilteam/scraper')
+const fetch = require('node-fetch');
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let caption = `
+    if (!args[0]) throw `*Example:* ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`;
+    
+    try {
+        const response = await fetch(`https://api.agatz.xyz/api/mediafire?url=${args[0]}`);
+        const json = await response.json();
+        
+        if (!json.result) throw 'Failed to fetch!';
+        
+        let { url, filename, ext, upload_date: aploud, filesize, filesizeH } = json.result;
+        
+        let caption = `
 *💌 Name:* ${filename}
 *📊 Size:* ${filesizeH}
 *🗂️ Extension:* ${ext}
 *📨 Uploaded:* ${aploud}
-`.trim()
-    m.reply(caption)
-     conn.sendMessage(m.chat, { document: { url: url }, mimetype: ext, fileName: filename }, { quoted: m })
-}
-handler.help = ['mediafire'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(mediafire|mf)$/i
-handler.register = true;
-handler.limit = true
+`.trim();
+        
+        m.reply(caption);
+        conn.sendMessage(m.chat, { document: { url: url }, mimetype: ext, fileName: filename }, { quoted: m });
+        
+    } catch (e) {
+        throw eror
+    }
+};
 
-module.exports = handler
+handler.help = ['mediafire'].map(v => v + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^(mediafire|mf)$/i;
+
+handler.limit = true;
+
+module.exports = handler;
